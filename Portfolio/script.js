8a1b3b084ff564f02f1105be94adb4b0958e72d2
vishalkumar5102 <105840751+vishalkumar5102@users.noreply.github.com>
2022-05-26 11:06:07 +0530

    first commit

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+// Typing animation
+
+var typed = new Typed(".typing",{
+    strings : [" ","Wordpress Developer","Shopify Developer","Web Developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+})
